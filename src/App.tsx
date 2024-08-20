9ccbfde6124eb562c66a9d69a946cedf2ef58eff
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-08-20 20:12:59 +0600

    23-7 Initialize useReducer inside provider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import CounterWithClass from "./components/CounterWithClassComponent";
-import CounterWithFuncComponent from "./components/CounterWithFuncComponent";
-import { useState } from "react";
+// import CounterWithClass from "./components/CounterWithClassComponent";
+// import CounterWithFuncComponent from "./components/CounterWithFuncComponent";
+// import { useState } from "react";
+
+// import CounterWithFuncComponent from "./components/CounterWithFuncComponent";
+import TodoForm from "./components/todo/TodoForm";
+import TodoProvider from "./context/TodoProvider";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   return (
-    <div className="border border-rose-400 m-10">
-      <div className="border border-fuchsia-400 p-2 m-2">
-        <CounterWithClass />
+    <TodoProvider>
+      <div>
+        <TodoForm/>
       </div>
+    </TodoProvider>
 
-      <div className="border border-y-emerald-400 m-2">{count}</div>
-      <div className="border border-zinc-400 m-2">
-        <CounterWithFuncComponent count={count} setCount={setCount} />
-      </div>
-    </div>
+    // <div className="border border-rose-400 m-10">
+    //   <div className="border border-fuchsia-400 p-2 m-2">
+    //     <CounterWithClass />
+    //   </div>
+
+    //   <div className="border border-y-emerald-400 m-2">{count}</div>
+    //   <div className="border border-zinc-400 m-2">
+    //     <CounterWithFuncComponent count={count} setCount={setCount} />
+    //   </div>
+    // </div>
   );
 }
 
