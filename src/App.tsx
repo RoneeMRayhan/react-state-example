cc4b15e184e2695670076a7746fe6f18d652c921
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-08-20 16:32:15 +0600

    state lift-up props-drilling one-way data binding

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import CounterWithClass from "./components/CounterWithClassComponent";
 import CounterWithFuncComponent from "./components/CounterWithFuncComponent";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-      <CounterWithClass />
-      <CounterWithFuncComponent />
+    <div className="border border-rose-400 m-10">
+      <div className="border border-fuchsia-400 p-2 m-2">
+        <CounterWithClass />
+      </div>
+
+      <div className="border border-y-emerald-400 m-2">{count}</div>
+      <div className="border border-zinc-400 m-2">
+        <CounterWithFuncComponent count={count} setCount={setCount} />
+      </div>
     </div>
   );
 }
--- src/components/CounterWithFuncComponent.tsx
+++ src/components/CounterWithFuncComponent.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react";
-
-const CounterWithFuncComponent = () => {
-  const [count, setCount] = useState(0);
-  return <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>;
+type TProps = {
+  count: number;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+};
+const CounterWithFuncComponent = ({ count, setCount }: TProps) => {
+  return (
+    <div className="border border-amber-500 m-10">
+      <button
+        className="border border-cyan-400 m-10"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        {count}
+      </button>
+    </div>
+  );
 };
 
 export default CounterWithFuncComponent;
