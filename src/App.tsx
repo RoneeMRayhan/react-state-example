9a2c98099a642a389f0cf7160706027bf1bb3f51
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-08-20 16:03:45 +0600

    O=> Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import CounterWithClass from "./components/CounterWithClassComponent";
+import CounterWithFuncComponent from "./components/CounterWithFuncComponent";
+
+function App() {
+  return (
+    <div>
+      <CounterWithClass />
+      <CounterWithFuncComponent />
+    </div>
+  );
+}
+
+export default App;
